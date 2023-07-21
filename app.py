3c15ef6ced854e5992d05bdacd59a9ed86b2e3c3
Sajjadlaghari <sajjadlaghari723@gmail.com>
2023-07-21 11:49:54 +0500

    Login signUp with db

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask,Response,session, request, render_template,flash,redirect, url_for
+from pymongo import MongoClient
+from bson.json_util import dumps
 
 app = Flask(__name__)
+
+MONGO_URI = "mongodb+srv://sajjad:Sajjad%40321@cluster0.xh2jqt2.mongodb.net/"
+client = MongoClient(MONGO_URI)
+db = client.get_database("Test") 
+collection =  db["user"]
+app.secret_key = "test11"
 
 
 @app.route('/')
@@ -15,11 +23,78 @@ def about():
 def contact():
     return render_template('contact-us.html')
 
-@app.route('/login')
-def login():
+@app.route('/login',methods=['GET'])
+def login_form():
     return render_template('login.html')
 
 
 @app.route('/register')
-def register():
+def register_form():
     return render_template('register.html')
+
+@app.route('/register',methods=['POST'])
+def register():
+     # Replace "<database>" with your database name
+
+    name = request.form['name']
+    email = request.form['email']
+    password = request.form['password']
+    collection =  db["user"]
+
+    user = collection.find_one({"email": email, "password": password})
+
+    if user: 
+        flash('Email Already taken', 'error')
+        return redirect(url_for('register'))
+    else:
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+        collection =  db["user"]
+        data = {"name": name, 'email':email,'password':password}
+        collection.insert_one(data)
+        flash('login Successfully', 'error')
+        return redirect(url_for('register'))
+
+@app.route('/login',methods=['POST'])
+def login():
+
+    email = request.form['email']
+    password = request.form['password']
+    user = collection.find_one({"email": email, "password": password})
+
+    if user:
+  
+        session['user'] = str(user["_id"])
+        session['name'] = str(user["name"])
+        session['email'] = str(user["email"])
+        flash('login Successfully', 'error')
+        return redirect(url_for('admin'))
+    else:
+        flash('"Invalid email or password.', 'error')
+        return redirect(url_for('register'))
+    
+@app.route('/admin',methods=['GET'])
+def admin():
+
+    if 'user' in session:
+        # Use the user_id to fetch user data from the database if needed
+       return render_template('admin-file.html')
+      
+    else:
+        flash('login First to Access this Page', 'error')
+        return redirect(url_for('login'))
+   
+
+@app.route('/logout')
+def logout():
+    if 'user' in session:
+        # Use the user_id to fetch user data from the database if needed
+       session.clear()
+       flash('Logout Successfully', 'error')
+       return redirect(url_for('login'))
+    else:
+        flash('Login First to Access this Page ', 'error')
+        return redirect(url_for('login'))
+   
+app.run(debug=True)
